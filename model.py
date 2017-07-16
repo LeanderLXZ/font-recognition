64c85e058919cf9d1c49a1db80d020d7b2141012
Lu Xuzheng <466532329@qq.com>
2017-07-16 16:00:49 +0800

    Add tf.summary support and 'log' directory.

--- model.py
+++ model.py
@@ -1,13 +1,18 @@
-import numpy as np
 import tensorflow as tf
 import pickle
+
+
+# Version
+
+version = '1.0'
 
 
 # Hyperparameters
 
 epochs = 10
 batch_size = 512
-learning_rate = 0.001
+learning_rate = 0.0002
+unit_num = 2048
 keep_prob = 0.5
 iteration_print = 10
 
@@ -44,19 +49,19 @@ labels_ = tf.placeholder(tf.int64, shape=[None, train_y.shape[1]], name='labels'
 # Classifier
 
 fc1 = tf.contrib.layers.fully_connected(inputs_,
-                                        1024,
+                                        unit_num,
                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                                         biases_initializer=tf.zeros_initializer())
 fc1 = tf.nn.dropout(fc1, keep_prob=keep_prob)
 
 fc2 = tf.contrib.layers.fully_connected(fc1,
-                                        512,
+                                        int(unit_num / 2),
                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                                         biases_initializer=tf.zeros_initializer())
 fc2 = tf.nn.dropout(fc2, keep_prob=keep_prob)
 
 fc3 = tf.contrib.layers.fully_connected(fc2,
-                                        256,
+                                        int(unit_num / 4),
                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                                         biases_initializer=tf.zeros_initializer())
 fc3 = tf.nn.dropout(fc3, keep_prob=keep_prob)
@@ -65,7 +70,11 @@ logits = tf.contrib.layers.fully_connected(fc3,
                                            train_y.shape[1],
                                            activation_fn=None)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels_, logits=logits))
+with tf.name_scope('cost'):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels_, logits=logits),
+                          name='cost')
+    tf.summary.scalar('cost', cost)
+
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
 
@@ -73,7 +82,11 @@ optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
 predicted = tf.nn.softmax(logits)
 correct_pred = tf.equal(tf.argmax(predicted, 1), tf.argmax(labels_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
+
+with tf.name_scope('accuracy'):
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32),
+                              name='accuracy')
+    tf.summary.scalar('accuracy', accuracy)
 
 
 # Get batches
@@ -99,19 +112,28 @@ def get_batches(x, y, n_batches):
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
+
+    merged = tf.summary.merge_all()
+
+    train_writer = tf.summary.FileWriter('./log/' + version + '/train', sess.graph)
+    val_writer = tf.summary.FileWriter('./log/' + version + '/val')
+
     sess.run(tf.global_variables_initializer())
     iteration = 1
 
-    # TODO: Your training code here
     for i in range(epochs):
         for batch_x, batch_y in get_batches(train_x, train_y, batch_size):
-            loss, train_acc, _ = sess.run([cost, accuracy, optimizer],
-                                       feed_dict={inputs_: batch_x,
-                                                  labels_: batch_y})
+            summary_train, loss, train_acc, _ = sess.run([merged, cost, accuracy, optimizer],
+                                                         feed_dict={inputs_: batch_x,
+                                                         labels_: batch_y})
+
+            train_writer.add_summary(summary_train, i)
 
             if iteration % iteration_print == 0:
-                val_acc = sess.run(accuracy, feed_dict={inputs_: val_x,
-                                                        labels_: val_y})
+                summary_val, val_acc = sess.run([merged, accuracy], feed_dict={inputs_: val_x,
+                                                                               labels_: val_y})
+
+                val_writer.add_summary(summary_val, i)
 
                 print('Epochs: {:>3} | Iteration: {:>5} | Loss: {:>9.4f} | Train_acc: {:>6.2f}% | Val_acc: {:.2f}%'
                       .format(i+1, iteration, loss, train_acc * 100, val_acc * 100))
